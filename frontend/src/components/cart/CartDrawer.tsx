529d9cd2911cc8278496422b57307dc5e72bdcee
jade <jadesada.jung@gmail.com>
2026-03-24 01:37:33 +0700

    fix build errors and layout merge

--- frontend/src/components/cart/CartDrawer.tsx
+++ frontend/src/components/cart/CartDrawer.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom"
 import { useCart } from "../../context/CartContext"
-import { useState } from "react"
 
 
 export default function CartDrawer({ open, onClose }: any) {
