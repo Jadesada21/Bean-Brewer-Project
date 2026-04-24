0308f07e0de25ad4f37d569160f7ac1e1f24478e
jade <jadesada.jung@gmail.com>
2026-04-24 18:54:53 +0700

    add rate limit on login route / helmet on App

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import Routes from './routes/index'
 import cors from 'cors'
 import { errorHandler } from './middleware/ErrorHandler'
 import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
 
 const app: Application = express()
 
@@ -13,6 +14,7 @@ app.use(cors({
 }))
 app.use(express.json({ limit: "50mb" }))
 
+app.use(helmet())
 app.use(Routes)
 
 app.use(errorHandler)
--- backend/src/routes/public-route/login-out-RegisRoute/login.route.ts
+++ backend/src/routes/public-route/login-out-RegisRoute/login.route.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
+import rateLimit from 'express-rate-limit'
 
 import {
     login
 } from '../../../controller/login.controller'
 
+const loginLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10 })
+
 const router = Router()
 
 router.route('/')
-    .post(login)
+    .post(loginLimiter, login)
 
 export default router
